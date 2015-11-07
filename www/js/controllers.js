a237148437053afbfa2f7f6b6c6b67103174a7dd
Khalil Malik <Khalilm1906@gmail.com>
2015-11-06 22:37:09 -0600

    added mapping capability

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,10 +28,40 @@ app.controller("YelpController", function ($scope, YelpService) {
 
   $scope.getDirections = function (cafe) {
     console.log("Getting directions for " + cafe.name);
+    var destination = [
+      cafe.location.coordinate.latitude,
+      cafe.location.coordinate.longitude
+    ];
+
+    var source = [
+      $scope.yelp.lat,
+      $scope.yelp.lon
+    ];
+
+    launchnavigator.navigate(destination, source);
   }
 
   $scope.openMap = function (cafe) {
     console.log("Opening Map");
+    var destination = [
+      cafe.location.coordinate.latitude,
+      cafe.location.coordinate.longitude
+    ];
+
+    launchnavigator.navigate(
+      destination,
+      undefined,
+    function(){
+      //alert("Plugin success");
+    },
+    function(error){
+      //alert("Plugin error: "+ error);
+    },
+    {
+      preferGoogleMaps: true,
+      disableAutoGeolocation: true
+    });
+
   }
 
 });
